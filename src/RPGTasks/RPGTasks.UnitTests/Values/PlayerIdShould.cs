ccb260b0ba30a675a1b548ae4b09652315453082
Guillem <guillemllompartpiza@gmail.com>
2020-10-02 00:45:35 +0100

    Create player id value type

---
+++ src/RPGTasks/RPGTasks.UnitTests/Values/PlayerIdShould.cs
@@ -0,0 +1,49 @@
+﻿namespace RPGTasks.UnitTests.Values
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RPGTasks.Values;
+    using System;
+
+    class PlayerIdShould
+    {
+        [Test]
+        public void AllowCreation()
+        {
+            var playerId = new PlayerId("id");
+            playerId.Value.Should().Be("id");
+        }
+
+        [Test]
+        public void PreventCreationWithNullValue()
+        {
+            try
+            {
+                new PlayerId(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PreventCreationWithEmptyValue()
+        {
+            try
+            {
+                new PlayerId("");
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/RPGTasks/RPGTasks/Values/PlayerId.cs
@@ -0,0 +1,15 @@
+﻿namespace RPGTasks.Values
+{
+    using System;
+
+    public class PlayerId
+    {
+        public string Value { get; }
+
+        public PlayerId(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            Value = value;
+        }
+    }
+}
