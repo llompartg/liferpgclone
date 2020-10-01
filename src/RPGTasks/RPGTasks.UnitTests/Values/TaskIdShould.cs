b121a36bd2b22c9196246ccea6c4e5ab8f6ec581
Guillem <guillemllompartpiza@gmail.com>
2020-10-02 00:47:31 +0100

    Create task id value type

---
+++ src/RPGTasks/RPGTasks.UnitTests/Values/TaskIdShould.cs
@@ -0,0 +1,49 @@
+﻿namespace RPGTasks.UnitTests.Values
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using RPGTasks.Values;
+    using System;
+
+    class TaskIdShould
+    {
+        [Test]
+        public void AllowCreation()
+        {
+            var taskId = new TaskId("id");
+            taskId.Value.Should().Be("id");
+        }
+
+        [Test]
+        public void PreventCreationWithNullValue()
+        {
+            try
+            {
+                new TaskId(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PreventCreationWithEmptyValue()
+        {
+            try
+            {
+                new TaskId("");
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/RPGTasks/RPGTasks/Values/TaskId.cs
@@ -0,0 +1,15 @@
+﻿namespace RPGTasks.Values
+{
+    using System;
+
+    public class TaskId
+    {
+        public string Value { get; }
+
+        public TaskId(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            Value = value;
+        }
+    }
+}
