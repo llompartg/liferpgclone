15df30d67da29a971ed44699d9f96b6140791a6f
Guillem <guillemllompartpiza@gmail.com>
2020-10-02 00:42:12 +0100

    Create task name value type

---
+++ src/RPGTasks/RPGTasks.UnitTests/Values/NameShould.cs
@@ -0,0 +1,49 @@
+﻿namespace RPGTasks.UnitTests.Values
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System;
+    using RPGTasks.Values;
+
+    class NameShould
+    {
+        [Test]
+        public void AllowCreation()
+        {
+            var taskName = new Name("name");
+            taskName.Value.Should().Be("name");
+        }
+
+        [Test]
+        public void PreventCreationWithNullValue()
+        {
+            try
+            {
+                new Name(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PreventCreationWithEmptyValue()
+        {
+            try
+            {
+                new Name("");
+            }
+            catch (ArgumentException ex)
+            {
+                ex.ParamName.Should().Be("value");
+                Assert.Pass();
+            }
+
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/RPGTasks/RPGTasks/Values/Name.cs
@@ -0,0 +1,16 @@
+﻿namespace RPGTasks.Values
+{
+    using System;
+
+    public class Name
+    {
+        public string Value { get; }
+
+        public Name(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            Value = value;
+
+        }
+    }
+}
